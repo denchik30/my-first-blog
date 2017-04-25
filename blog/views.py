920561ed089d49f10591f9b9eda351fb71ef9469
den <mr.deonis@gmail.com>
2017-04-25 21:50:40 +0200

    Added author of post

--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,7 @@ from django.shortcuts import render_to_response
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	
 	return render(request, 'blog/post_list.html',{'posts':posts})
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
